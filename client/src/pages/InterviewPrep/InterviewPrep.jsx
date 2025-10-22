a31c656ca5b9fb29e024689b6e4ba8f171900095
Hemika Ghag <ghaghemika@gmail.com>
2025-10-22 19:14:34 +0530

    More Error Fixes

--- client/src/pages/InterviewPrep/InterviewPrep.jsx
+++ client/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { useParams } from "react-outer-dom";
+import { useParams } from "react-router-dom";
 import moment from "moment";
 import { AnimatePresence, motion } from "framer-motion";
 import { LuCircleAlert, LuListCollapse } from "react-icons/lu";
 import SpinnerLoader from "../../components/Loader/SpinnerLoader";
 import { toast } from "react-hot-toast";
-import DashboardLayout from '../../components/layouts/DashboardLayout';
-import RoleInfoHeader from './components/RoleInfoHeader';
-import axiosInstance from '../../utils/axiosInstance';
-import { API_PATHS } from '../../utils/apiPaths';
-import QuestionCard from '../../components/Cards/QuestionCard';
-import Drawer from '../../components/Drawer';
-import SkeletonLoader from '../../components/Loader/SkeletonLoader';
-import AIResponsePreview from './components/AIResponsePreview';
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import RoleInfoHeader from "./components/RoleInfoHeader";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import QuestionCard from "../../components/Cards/QuestionCard";
+import Drawer from "../../components/Drawer";
+import SkeletonLoader from "../../components/Loader/SkeletonLoader";
+import AIResponsePreview from "./components/AIResponsePreview";
 
 const InterviewPrep = () => {
   const { sessionId } = useParams();
@@ -20,8 +20,8 @@ const InterviewPrep = () => {
   const [sessionData, setSessionData] = useState(null);
   const [errorMsg, setErrorMsg] = useState("");
 
-  const [openLeanMoreDrawer, setOpenLeanMoreDrawer] = useState(false);
-  const [explanaion, setExplanation] = useState(null);
+  const [openLearnMoreDrawer, setOpenLearnMoreDrawer] = useState(false);
+  const [explanation, setExplanation] = useState(null);
 
   const [isLoading, setIsLoading] = useState(false);
   const [isUpdateLoader, setIsUpdateLoader] = useState(false);
@@ -30,7 +30,7 @@ const InterviewPrep = () => {
   const fetchSessionDetailsById = async () => {
     try {
       const response = await axiosInstance.get(
-        API_PATHS.SESSION.GYE_ONE(sessionID)
+        API_PATHS.SESSION.GET_ONE(sessionId)
       );
 
       if (response.data && response.data.session) {
@@ -42,10 +42,10 @@ const InterviewPrep = () => {
   };
 
   //Generate Concept Explanation
-  const generationConceptExplanation = async (question) => {
+  const generateConceptExplanation = async (question) => {
     try {
       setErrorMsg("");
-      setExplanation(null)
+      setExplanation(null);
 
       setIsLoading(true);
       setOpenLeanMoreDrawer(true);
@@ -54,21 +54,18 @@ const InterviewPrep = () => {
         API_PATHS.AI.GENERATE_EXPLANATION,
         {
           question,
-
         }
       );
 
       if (response.data) {
         setExplanation(response.data);
       }
-      } catch (error) {
-        setExplanation(null)
-        setErrorMsg("Failed to generate explanation, Try again later");
-        console.error("Error:", error);
-      } finally {
-        setIsLoading(false);
-
-      }
+    } catch (error) {
+      setExplanation(null);
+      setErrorMsg("Failed to generate explanation, Try again later");
+      console.error("Error:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -81,17 +78,13 @@ const InterviewPrep = () => {
 
       console.log(response);
 
-      if(response.data && response.data.question) {
+      if (response.data && response.data.question) {
         //toast.success('Question Pinned Succecssfully')
         fetchSessionDetailsById();
-
       }
-
     } catch (error) {
       console.error("Error:, error");
-
     }
-
   };
 
   //Add more questions to a session
@@ -106,7 +99,7 @@ const InterviewPrep = () => {
           role: sessionData?.role,
           experience: sessionData?.experience,
           topicsToFocus: sessionData?.topicsToFocus,
-          njumberOfQuestions: 10,
+          numberOfQuestions: 10,
         }
       );
       // Should be Array like[{question, answer}, ...]
@@ -120,10 +113,9 @@ const InterviewPrep = () => {
         }
       );
 
-      if(response.data) {
-        toast.succcess("Added More Q&A!!");
+      if (response.data) {
+        toast.success("Added More Q&A!!");
         fetchSessionDetailsById();
-
       }
     } catch (error) {
       if (error.response && error.response.data.message) {
@@ -131,7 +123,7 @@ const InterviewPrep = () => {
       } else {
         setError("Something went wrong. Please try again");
       }
-    } finally{
+    } finally {
       setIsUpdateLoader(false);
     }
   };
@@ -143,62 +135,60 @@ const InterviewPrep = () => {
 
     return () => {};
   }, []);
+
   return (
     <DashboardLayout>
       <RoleInfoHeader
         role={sessionData?.role || ""}
         topicsToFocus={sessionData?.topicsToFocus || ""}
         experience={sessionData?.experience || "-"}
-        questions={sessionData?.question?.length || "-"}
+        questions={sessionData?.questions?.length || "-"}
         description={sessionData?.description || ""}
         lastUpdated={
           sessionData?.updatedAt
-            ? moment(sessionData.updateAt).format("Do MMM YYYY")
-             : ""
+            ? moment(sessionData.updatedAt).format("Do MMM YYYY")
+            : ""
         }
       />
-    
+
       <DashboardLayout className="container mx-auto pt-4 pb-4 px-4 md:px-0">
         <h2 className="text-lg font-semibold color-black">Interview Q & A</h2>
 
         <div className="grid grid-cols-12 gap-4 mt-5 mb-10">
           <div
             className={`col-span-12 ${
-               openLeanMoreDrawer ? "md:col-end-7" : "md:col-end-8"
+              openLeanMoreDrawer ? "md:col-end-7" : "md:col-end-8"
             }`}
           >
             <AnimatePresence>
-             {sessionData?.questions?.map((data,index)=> {
-               return(
-                <motion.div  
+              {sessionData?.questions?.map((data, index) => (
+                <motion.div
                   key={data._id || index}
                   initial={{ opacity: 0, y: -20 }}
                   animate={{ opacity: 1, y: 0 }}
                   exit={{ opacity: 0, scale: 0.95 }}
-                  transition= {{
+                  transition={{
                     duration: 0.4,
                     type: "spring",
-                    stiffness:100,
+                    stiffness: 100,
                     delay: index * 0.1,
                     damping: 15,
                   }}
-                  layout // This is the key prop thjat animates position changes
-                  layoutId={`question-${data._id || index}`} //Helps framer track specific items 
+                  layout
+                  layoutId={`question-${data._id || index}`}
                 >
-                  <motion.div>
-                    <QuestionCard
-                      question={data?.question}
-                      answer={data?.answer}
-                      onLearnMore={() => 
-                        generateConceptExplanation(data.question)
-                      }
-                      isPinned={data?.isPinned}
-                      onTogglePin={() => toggleQuestionPinStatus(data._id)}
-                    />
-                  
+                  <QuestionCard
+                    question={data?.question}
+                    answer={data?.answer}
+                    onLearnMore={() =>
+                      generateConceptExplanation(data.question)
+                    }
+                    isPinned={data?.isPinned}
+                    onTogglePin={() => toggleQuestionPinStatus(data._id)}
+                  />
 
-                 {!isLoading &&
-                     sessionData?.questions?.lenght == index + 1 && (
+                  {!isLoading &&
+                    sessionData?.questions?.length === index + 1 && (
                       <div className="flex items-center justify-center mt-5">
                         <button
                           className="flex items-center gap-3 text-sm text-white font-medium bg-black px-5 py-2 mr-2 rounded text-nowrap cursor-pointer"
@@ -210,39 +200,36 @@ const InterviewPrep = () => {
                           ) : (
                             <LuListCollapse className="text-lg" />
                           )}{" "}
-                          Load More 
-                          </button>
-                        </div>   
-                        )}  
-                    </>                 
-                </motion.div> 
-              );
-             })}
-            </AnimatePresence> 
+                          Load More
+                        </button>
+                      </div>
+                    )}
+                </motion.div>
+              ))}
+            </AnimatePresence>
           </div>
         </div>
 
-
-          <div>
-            <Drawer 
-              isOpen={openLeanMoreDrawer}
-              onClose={() => setOpenLeanMoreDrawer(false)}
-              title={!isLoading && explanation?.title}
-            >
-              {errorMsg && (
-                <p className="flex gap-2 text-sm text-amber-600 font-medium">
-                  <LuCircleAlert className="mt-1" /> {errorMsg}
-               </p>
-              )}
-              {isLoading && <SkeletonLoader />}
-              {!isLoading && explanation && (
-                <AIResponsePreview content={explanation?.explanation}/>
-              )}
-            </Drawer>  
-          </div>
+        <div>
+          <Drawer
+            isOpen={openLearnMoreDrawer}
+            onClose={() => setOpenLearnMoreDrawer(false)}
+            title={!isLoading && explanation?.title}
+          >
+            {errorMsg && (
+              <p className="flex gap-2 text-sm text-amber-600 font-medium">
+                <LuCircleAlert className="mt-1" /> {errorMsg}
+              </p>
+            )}
+            {isLoading && <SkeletonLoader />}
+            {!isLoading && explanation && (
+              <AIResponsePreview content={explanation?.explanation} />
+            )}
+          </Drawer>
+        </div>
       </DashboardLayout>
     </DashboardLayout>
   );
+};
 
-
-export default InterviewPrep
+export default InterviewPrep;
